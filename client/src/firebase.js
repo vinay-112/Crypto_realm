25493f428d552ccafd9d7b78bea2fea0954c3509
Vinay sharma <vinaynotbinay@gmail.com>
2024-05-18 01:19:20 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,21 +1,4 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: 'mern-blog-b327f.firebaseapp.com',
-//   projectId: 'mern-blog-b327f',
-//   storageBucket: 'mern-blog-b327f.appspot.com',
-//   messagingSenderId: '699397991367',
-//   appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
 
 
 
@@ -29,7 +12,8 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwYzADwsr5U0YDuuyk7Its0IapSupSx5Y",
+  // apiKey: "AIzaSyAwYzADwsr5U0YDuuyk7Its0IapSupSx5Y",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY
   authDomain: "algotrading-f83ea.firebaseapp.com",
   projectId: "algotrading-f83ea",
   storageBucket: "algotrading-f83ea.appspot.com",
